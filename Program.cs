e89abdf11b7c43bda0dd643f9581c7e8c1055412
DiaaHSharqawi <162372579+DiaaHSharqawi@users.noreply.github.com>
2024-09-21 12:16:04 +0300

    C#_MVC_Auth

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using crud.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<appDbContext>(
+            options => options.UseSqlServer(builder.Configuration.GetConnectionString("defultConection"))
+            );
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
